003cfc7e02b7bc8cbd0d656fe38f326f11cd3168
lmnitzsche <logannitzsche1@gmail.com>
2025-02-21 22:26:04 -0600

    improved testimonial component

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,24 @@ document.addEventListener("DOMContentLoaded", function() {
         "retina_detect": true
     });
 });
+
+$('.carousel-testimonial').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
