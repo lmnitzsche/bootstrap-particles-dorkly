f451970b63fe90169c0662e30060bc74895f18c2
lmnitzsche <logannitzsche1@gmail.com>
2025-02-21 18:14:51 -0600

    made UI changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     particlesJS("particles-js", {
         "particles": {
-            "number": { "value": 100, "density": { "enable": true, "value_area": 800 } },
+            "number": { "value": 200, "density": { "enable": true, "value_area": 800 } },
             "color": { "value": "#ffffff" },
             "shape": { "type": "circle" },
             "opacity": { "value": 0.5, "random": true },
